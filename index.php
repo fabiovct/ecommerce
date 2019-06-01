8d3d2209da192ee32c72e2360ea602ee17dc18d2
Fábio Vicentini <vct.fabio.vct@gmail.com>
2019-05-31 22:58:03 -0300

    alterações

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
+require_once("functions.php");
+
+use Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -8,7 +11,52 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	//$page = new Hcode\Page();
+
+	//$page->setTpl("index");
+
+	$sql = new Hcode\DB\Sql();
+	$results = $sql->select("SELECT*FROM tb_users");
+	echo json_encode($results);
+
+});
+
+$app->get('/admin', function() {
+    
+	User::verifyLogin();
+
+	$page = new Hcode\PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login(post('deslogin'), post('despassword'));
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit1fdf074129504563c921dfae9320a2a2
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit1fdf074129504563c921dfae9320a2a2
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit1fdf074129504563c921dfae9320a2a2::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit1fdf074129504563c921dfae9320a2a2::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit1fdf074129504563c921dfae9320a2a2::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit1fdf074129504563c921dfae9320a2a2::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit1fdf074129504563c921dfae9320a2a2::$classMap;
 
