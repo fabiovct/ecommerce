4707fca0bd67f84a82875b4cc052a571fab089a1
Fábio Vicentini <vct.fabio.vct@gmail.com>
2019-06-01 19:53:40 -0300

    criando class page and pagina index

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php 
-session_start();
+//session_start();
 require_once("vendor/autoload.php");
-require_once("functions.php");
+//require_once("functions.php");
 
-use Hcode\Model\User;
+use\Slim\Slim;
+//use Hcode\Model\User;
+use Hcode\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
 	//$page = new Hcode\Page();
+	$page = new Page();
 
 	//$page->setTpl("index");
-
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT*FROM tb_users");
-	echo json_encode($results);
+	$page->setTpl("index");
 
 });
 
